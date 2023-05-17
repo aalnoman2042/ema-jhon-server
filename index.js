92dd9ef6417a698c796c8993920e7eb359cfad97
aalnoman <abdullahalnoman2042@gmail.com>
2023-05-17 20:01:46 +0600

    pagination

--- index.js
+++ index.js
@@ -9,6 +9,66 @@ app.use(cors())
 app.use(express.json())
 
 
+// mongoDb
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d4rcuph.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const productCollection = client.db('emaJhonDB').collection('products')
+        // all products in database 
+    app.get('/products', async(req, res)=>{
+        console.log(req.query);
+        const page = parseInt(req.query.page) || 0
+        const limit = parseInt(req.query.limit) || 10
+        const skip = page* limit
+        const result = await productCollection.find().skip(skip).limit(limit).toArray()
+        res.send(result)
+    })
+
+// 
+
+    app.get('/totalProducts', async (req, res) => {
+        const result = await productCollection.estimatedDocumentCount();
+        res.send({ totalProducts: result })
+      })
+
+    // cart priducts by id
+    app.post('/productsByIds', async(req, res) => {
+        const ids = req.body;
+        const objectIds = ids.map(id => new ObjectId(id));
+        const query = { _id: { $in: objectIds } }
+        
+        const result = await productCollection.find(query).toArray();
+        res.send(result);
+      })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 // home route
  app.get('/', (req, res)=>{
     res.send('jhon is busy shopping')
